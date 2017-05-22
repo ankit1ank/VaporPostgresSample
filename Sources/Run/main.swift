3a30f2307421681fefe241e0cefe75ac8cdf59ea
Ankit Goel <ankit1ank@gmail.com>
2017-05-23 02:05:38 +0530

    Initial commit

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,25 +1,16 @@
 import App
+import PostgreSQLDriver
 
-/// We have isolated all of our App's logic into
-/// the App module because it makes our app
-/// more testable.
-///
-/// In general, the executable portion of our App
-/// shouldn't include much more code than is presented
-/// here.
-///
-/// We simply initialize our Droplet, optionally
-/// passing in values if necessary
-/// Then, we pass it to our App's setup function
-/// this should setup all the routes and special
-/// features of our app
-///
-/// .run() runs the Droplet's commands, 
-/// if no command is given, it will default to "serve"
 let config = try Config()
 try config.setup()
 
 let drop = try Droplet(config)
 try drop.setup()
+
+drop.get("version") { request in
+    let db = try drop.postgresql()
+    let version = try db.raw("SELECT version()")
+    return try JSON(node: ["version": version])
+}
 
 try drop.run()
